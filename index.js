02527dd7857ddc4df52cc51c8541155cad65143d
IsidineJJ <isidinedjisnaba1@gmail.com>
2025-06-25 16:48:01 +0000

    2de dépôt

--- index.js
+++ index.js
@@ -1,37 +1,15 @@
 const navLink1 = document.getElementById("link-1");
-let activeLink = navLink1.id;
 const navLinks = document.querySelectorAll(".nav-link");
-const linksSize = navLinks.length; //la taille des liens
 
-for (let i = 0; i < linksSize; i++) {
-  const link = navLinks[i];
-  //récupérer idlink
-  const linkId = link.id;
-  if (activeLink === linkId) {
-    link.classList.add("nav-link-active");
-  }
-}
+navLink1.classList.add("nav-link-active");
 
-for (let i = 0; i < linksSize; i++) {
-  const link = navLinks[i];
+navLinks.forEach((element) => {
+  element.addEventListener("click", () => {
+    navLinks.forEach((element) => element.classList.remove("nav-link-active"));
 
-  //récupérer idlink
-  const linkId = link.id;
-
-  //attacher l'évènement clickc sur le link
-  link.addEventListener("click", (event) => {
-    console.log("bonjour le monde");
-    linkClickListener(event, link);
+    element.classList.add("nav-link-active");
   });
-}
-
-function linkClickListener(event, link) {
-  const clickedElement = event.target;
-  activeLink = clickedElement.id;
-  if (activeLink === link.id) {
-    link.classList.add("nav-link-active");
-  }
-}
+});
 
 /*
 if (condition) {
